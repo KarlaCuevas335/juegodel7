789462bc339eda4a02e6e097e5fa877e37f67f01
Celula <karlitacuevas2001@gmail.com>
2019-10-16 12:47:28 -0500

    First Commit

---
+++ TDA/Dado.py
@@ -0,0 +1,11 @@
+import random
+class Dado:
+    def __init__(self):
+        self.dado=o
+
+    def tirarDado(self):
+        self.valor= random.randint(1, 6)
+
+    def getValor(self):
+        return self.valor
+    
